3ef1d9ab85ffd749df10ee15211988155b2c9c72
Mehmet Can CİCEK <m.c.cicek@hotmail.com>
2018-09-14 08:12:41 +0300

    GameCom

---
+++ Hİkaye tabanlı deneme/Assets/Hareket.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hareket : MonoBehaviour {
+
+    public Camera Cam;
+    public Button sagButton;
+    public Button solButton;
+    public Text text;
+	// Use this for initialization
+
+        void OnEnable()
+    {
+        sagButton.onClick.AddListener(() => değiştir());
+    }
+    
+    void değiştir()
+    {
+        text.GetComponent<Text>().text = "dsdsdsddsdsds";
+        Cam.GetComponent<Transform>().position = new Vector3(-500,-17,-20);
+       
+        Debug.Log("dwdw");
+    }
+
+
+	void Start () {
+	
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ RollABall/Assets/Script/RotateIt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateIt : MonoBehaviour {
+
+    public float rotateX;
+    public float rotateY;
+    public float rotateZ;
+
+    void Awake()
+    {
+        rotateX = Random.Range(10f, 50f);
+        rotateY = Random.Range(10f, 50f);
+        rotateZ = Random.Range(10f, 50f);
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(rotateX, rotateY, rotateZ) * Time.deltaTime);
+    }
+
+}
