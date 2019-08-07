c9742153e3aaae443a3317ed8633ca2d2dcc1754
Mehmet Can CİCEK <m.c.cicek@hotmail.com>
2019-08-07 21:17:15 +0300

    Eklemeler

---
+++ Recursive/Assets/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    GameObject prefab;
+    void Start()
+    {
+        if (transform.localScale.x > 0.1f)
+        {
+            prefab = Resources.Load("Cube") as GameObject;
+            prefab.transform.localScale = transform.localScale / 2f;
+            Instantiate(prefab, transform.position + transform.localScale , Quaternion.identity);
+            Instantiate(prefab, transform.position + transform.localScale , Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
